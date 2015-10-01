ef623384a1748b96b932ca75643f830edd7847fd
Agustín de Cabrera <Agustin.DeCabrera@rga.com>
2015-10-01 19:30:59 -0300

    Added framework target for Carthage compatibility

---
+++ SWTableViewCellFramework/SWTableViewCellFramework.h
@@ -0,0 +1,17 @@
+//
+//  SWTableViewCellFramework.h
+//  SWTableViewCellFramework
+//
+//  Created by Agustín de Cabrera on 10/1/15.
+//  Copyright © 2015 Chris Wendel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWTableViewCellFramework.
+FOUNDATION_EXPORT double SWTableViewCellFrameworkVersionNumber;
+
+//! Project version string for SWTableViewCellFramework.
+FOUNDATION_EXPORT const unsigned char SWTableViewCellFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWTableViewCellFramework/PublicHeader.h>
